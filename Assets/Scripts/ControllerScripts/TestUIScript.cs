d3ad4619de16aeca25b8fe94f080e0d154ff69f4
FilipJinstrand <filip@jinstrand.se>
2018-09-05 16:01:54 +0200

    Started With UI

---
+++ Assets/Scripts/ControllerScripts/TestUIScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUIScript : MonoBehaviour {
+
+    private float ControllerRotation;
+    public GameObject Controller;
+
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ControllerRotation = Controller.transform.rotation.z;
+
+        if (ControllerRotation >= 100)
+        {
+            
+        }
+    }
+}
