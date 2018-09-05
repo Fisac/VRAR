0bde9a1fcf88936d08e4cd8b3d4d0e5da6e063a4
Fisac <f-isac@hotmail.com>
2018-09-05 10:36:18 +0200

    Added ManipulationManager

---
+++ Assets/Scripts/ManipulationSystem/ManipulationManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipulationManager : MonoBehaviour {
+
+    private GameObject[] prop;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        
+	}
+}
