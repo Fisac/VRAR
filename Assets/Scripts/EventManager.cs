06e3a91d05c6f4fee7bc1264f9dfd296146e11d3
Unknown <chris_albin@outlook.com>
2018-09-10 14:54:26 +0200

    WIP Destruction

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class EventManager : MonoBehaviour {
+
+
+    [SerializeField]
+    public GameObject test; 
+    // Use this for initialization
+    public void DestroyBlock()
+    {
+        //propertyTriggered[0].Invoke();
+        StartCoroutine("Destruction"); 
+    }
+
+    IEnumerator Destruction()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(test); 
+    }
+}
