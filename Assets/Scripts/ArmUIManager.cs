9af7aba8b8594d1611b15ac382b4e65dbe7a5d1e
Joakim <jokenordstrom@hotmail.com>
2018-09-10 15:22:10 +0200

    framework for UIactivation

---
+++ Assets/Scripts/ArmUIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmUIManager : MonoBehaviour {
+
+    public Canvas armUICanvas;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="MainCamera")
+        {
+            armUICanvas.gameObject.SetActive(true);
+            Debug.Log("MainCamera in trigger.");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            armUICanvas.gameObject.SetActive(false);
+            Debug.Log("MainCamera not in trigger.");
+        }
+    }
+}
