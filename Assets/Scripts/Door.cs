43c21b4cd50ed888e7ee8616b7d2000479b7de3d
Joakim <jokenordstrom@hotmail.com>
2018-09-10 13:03:30 +0200

    sync with Isaac

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Transform doorTransform;
+
+    float doorStart;
+    float doorStop;
+
+    float lerpTime = 0f;
+
+    void Start()
+    {
+        doorTransform = GetComponent<Transform>();
+
+        doorStart = doorTransform.position.z;
+        doorStop = doorTransform.position.z - 0.1f;
+    }
+
+    void OpenDoor()
+    {
+        lerpTime += 0.5f * Time.deltaTime;
+        doorTransform.position = new Vector3(doorTransform.position.x, doorTransform.position.y, Mathf.Lerp(doorStart, doorStop, lerpTime));
+
+        //Destroy(this);
+    }
+}
