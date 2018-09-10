986252e3d4d4f6ff901f429c565cab02a63734a8
Unknown <chris_albin@outlook.com>
2018-09-10 16:45:55 +0200

    WIP Collision

---
+++ Assets/Scripts/ManipulationSystem/DestructibleObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour {
+
+    private Matter matter;
+    private MeshRenderer mesh;
+    private BoxCollider collider; 
+
+
+    private void Awake()
+    {
+        matter = gameObject.GetComponent<Matter>();
+        mesh = gameObject.GetComponent<MeshRenderer>();
+        collider = gameObject.GetComponent<BoxCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        StartCoroutine("DesroyObject");
+    }
+
+    IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(0.5f);
+        mesh.enabled = false;
+        collider.enabled = false; 
+    }
+}
