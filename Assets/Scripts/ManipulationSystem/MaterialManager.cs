f44512a3a33181c72d0bd9af6f8cca0693b12868
Unknown <chris_albin@outlook.com>
2018-09-11 11:21:16 +0200

    Fixed prefab and WIP MaterialManager

---
+++ Assets/Scripts/ManipulationSystem/MaterialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour {
+
+    private InteractableObject interactable;
+    private Matter currentMatter;
+
+    private Renderer rend; 
+
+    private void Awake()
+    {
+        interactable = gameObject.GetComponent<InteractableObject>();
+        rend = gameObject.GetComponent<Renderer>();
+        currentMatter = interactable.matter;
+        rend.material = currentMatter.matterMaterial; 
+       
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ApplyNewMaterial();
+		
+	}
+
+    public void ApplyNewMaterial()
+    {
+        if(rend.material != currentMatter.matterMaterial)
+        {
+            rend.material = currentMatter.matterMaterial; 
+        }
+    }
+}
