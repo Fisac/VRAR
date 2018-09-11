15418783c04db257947dc300648229944cb55c1f
Unknown <chris_albin@outlook.com>
2018-09-11 10:16:33 +0200

    Working Destructible (sans Dissolve)
    TODO: Implement dissolve.

--- Assets/Scripts/ManipulationSystem/DestructibleObject.cs
+++ Assets/Scripts/ManipulationSystem/DestructibleObject.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class DestructibleObject : MonoBehaviour {
 
-    private Matter matter;
+    [SerializeField]
+    private float transitionDuration = 0.0f; 
+    private InteractableObject interactable; 
     private MeshRenderer mesh;
-    private BoxCollider collider; 
-
+    private BoxCollider collider;
+    const string destroyObject = "DestroyObject";
 
     private void Awake()
     {
-        matter = gameObject.GetComponent<Matter>();
+        interactable = gameObject.GetComponent<InteractableObject>();
         mesh = gameObject.GetComponent<MeshRenderer>();
         collider = gameObject.GetComponent<BoxCollider>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
-        StartCoroutine("DesroyObject");
+       if(interactable.matter.isDestructable)
+        {
+            StartCoroutine(destroyObject);
+        }
+       else
+        {
+            return; 
+        }
     }
 
     IEnumerator DestroyObject()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(transitionDuration);
         mesh.enabled = false;
         collider.enabled = false; 
     }
