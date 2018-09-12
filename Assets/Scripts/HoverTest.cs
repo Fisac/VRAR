042ecfbad7298c7f2d56f0ac37941208d56b5996
Joakim <jokenordstrom@hotmail.com>
2018-09-12 10:12:00 +0200

    fixed "billboard effect"

---
+++ Assets/Scripts/HoverTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverTest : MonoBehaviour {
+
+    public VRTK.VRTK_InteractableObject vrtkInteractable;
+
+    public Canvas testUIElement;
+
+    Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (vrtkInteractable.isActiveAndEnabled && !myRigidbody.isKinematic)
+        {
+            testUIElement.gameObject.SetActive(true);
+        }
+        else
+        {
+            testUIElement.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+    public Camera vrCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (FindObjectOfType<Camera>().tag=="MainCamera")
+        {
+            vrCamera = GetComponent<Camera>();	
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.LookAt(transform.position + vrCamera.transform.rotation * Vector3.forward, vrCamera.transform.rotation * Vector3.up);
+        transform.LookAt(vrCamera.transform.position);
+    }
+}
