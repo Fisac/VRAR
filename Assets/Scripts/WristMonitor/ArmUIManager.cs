d441c261bce14ca43776eb7e3d59f41be91f4d8e
Fisac <isac_degerborg@hotmail.com>
2018-09-12 13:39:33 +0200

    Added wristmonitor stuff

--- Assets/Scripts/WristMonitor/ArmUIManager.cs
+++ Assets/Scripts/WristMonitor/ArmUIManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ArmUIManager : MonoBehaviour {
 
     public Canvas armUICanvas;
+    public GameObject controller, hand;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag=="MainCamera")
         {
             armUICanvas.gameObject.SetActive(true);
+            //controller.SetActive(false);
+            //hand.SetActive(false);
             Debug.Log("MainCamera in trigger.");
         }
     }
@@ -20,6 +23,8 @@ public class ArmUIManager : MonoBehaviour {
         if (other.tag == "MainCamera")
         {
             armUICanvas.gameObject.SetActive(false);
+            controller.SetActive(true);
+            hand.SetActive(true);
             Debug.Log("MainCamera not in trigger.");
         }
     }
---
+++ Assets/Scripts/WristMonitor/RotateUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateUI : MonoBehaviour {
+
+    public GameObject head;
+
+    private void LateUpdate()
+    {
+        transform.LookAt(head.transform);
+    }
+}
